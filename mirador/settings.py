031da7a407e94c5f783921040bc5c12e0f99eb5e
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-01-19 22:40:45 -0500

    Add storage models

--- mirador/settings.py
+++ mirador/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'detection',
     'overlay',
     'schedule',
+    'storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from camera import models as camera
+
+
+class Video(models.Model):
+    camera = models.ForeignKey(camera.Camera, null=True, blank=True, on_delete=models.SET_NULL)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    file = models.FilePathField()
+
+
+class Picture(models.Model):
+    camera = models.ForeignKey(camera.Camera, null=True, blank=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField()
+    file = models.FilePathField()
+
+
+class Event(models.Model):
+    date = models.DateTimeField()
+    type = models.CharField(max_length=8)
+    video = models.ForeignKey(Video, null=True, blank=True, on_delete=models.SET_NULL)
+    picture = models.ForeignKey(Picture, null=True, blank=True, on_delete=models.SET_NULL)
+    data = models.JSONField(null=True, blank=True)
