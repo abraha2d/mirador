56ca94b57389b1e1642094a3ab20943ebb3f325a
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-01-22 21:48:08 -0500

    Update overlay admin

--- overlay/models.py
+++ overlay/models.py
@@ -4,8 +4,9 @@ from colorfield.fields import ColorField
 
 
 class Overlay(models.Model):
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=True)
     format = models.CharField(max_length=255)
     x_pos = models.PositiveSmallIntegerField()
     y_pos = models.PositiveSmallIntegerField()
-    color = ColorField(format='hexa')
+    foreground = ColorField(format='hexa', default='#FFF')
+    background = ColorField(format='hexa', default='#000')
