dce5d803001809e6bed77745a84368ef0be7bef3
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-01-22 22:04:14 -0500

    Customize admin site header

--- mirador/urls.py
+++ mirador/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "Mirador administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
