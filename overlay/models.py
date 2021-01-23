865d329ab00f9a42d41044b19357c50d7d342450
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-01-23 00:18:43 -0500

    Allow negative overlay positions

--- overlay/models.py
+++ overlay/models.py
@@ -6,7 +6,10 @@ from colorfield.fields import ColorField
 class Overlay(models.Model):
     enabled = models.BooleanField(default=True)
     format = models.CharField(max_length=255)
-    x_pos = models.PositiveSmallIntegerField()
-    y_pos = models.PositiveSmallIntegerField()
+    x_pos = models.SmallIntegerField()
+    y_pos = models.SmallIntegerField()
     foreground = ColorField(format='hexa', default='#FFF')
     background = ColorField(format='hexa', default='#000')
+
+    def __str__(self):
+        return f"{self.format} @ ({self.x_pos}, {self.y_pos})"
