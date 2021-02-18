9232a5f6fbd48ddc9536f7a2fb14ec409a93aca2
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-02-18 00:58:09 -0500

    Implement dragging streams between squares

--- ui/src/components/StreamContainer/StreamContainer.tsx
+++ ui/src/components/StreamContainer/StreamContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDrop } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
 
 import { START_STREAM } from "components/Store/constants";
 import { Stream } from "components/Store/types";
@@ -23,37 +23,60 @@ export const StreamContainer = ({
   dispatch,
 }: StreamContainerProps) => {
   const [{ isOver }, drop] = useDrop({
-    accept: DragItemTypes.CAMERA,
+    accept: [DragItemTypes.CAMERA, DragItemTypes.STREAM],
     drop: (item) => {
-      const camera = (item as any).camera;
-      dispatch &&
-        dispatch({
-          type: START_STREAM,
-          payload: {
-            idx,
-            stream: { id: camera.id, url: camera.name },
-          },
-        });
+      if (item.type === DragItemTypes.CAMERA) {
+        const camera = (item as any).camera;
+        dispatch &&
+          dispatch({
+            type: START_STREAM,
+            payload: {
+              idx,
+              stream: { id: camera.id, url: camera.name },
+            },
+          });
+      } else if (item.type === DragItemTypes.STREAM) {
+        const stream = (item as any).stream;
+        dispatch &&
+          dispatch({
+            type: START_STREAM,
+            payload: {
+              idx,
+              stream,
+            },
+          });
+      }
     },
     collect: (monitor) => ({
       isOver: !monitor.isOver(),
     }),
   });
 
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: DragItemTypes.STREAM, stream },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    canDrag: !!stream,
+  });
+
   return (
     <div
-      className={`d-flex flex-column align-items-center justify-content-center border-top border-left${
-        isOver ? "" : " bg-primary"
+      className={`border-top border-left${
+        isDragging ? " bg-info" : isOver ? "" : " bg-primary"
       }`}
       style={{ width, height }}
       ref={drop}
     >
       {stream && (
-        <>
+        <div
+          className="w-100 h-100 d-flex flex-column align-items-center justify-content-center"
+          ref={drag}
+        >
           <span className="text-light">ID: {stream.id}</span>
           <span className="text-light">URL: {stream.url}</span>
           <img src={logo} className="live w-50 h-50" alt="" />
-        </>
+        </div>
       )}
     </div>
   );
