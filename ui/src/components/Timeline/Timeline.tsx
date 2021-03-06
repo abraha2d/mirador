d014e53d43d5bfbcfb0622fcce021796c0c48b0e
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-03-06 18:38:47 -0500

    Add zoom in/out, transitions

--- ui/src/components/Timeline/Timeline.tsx
+++ ui/src/components/Timeline/Timeline.tsx
@@ -1,6 +1,6 @@
 import { useContext, useRef, useState } from "react";
-import { Button, OverlayTrigger, Popover } from "react-bootstrap";
-import { Calendar3, SkipEndFill } from "react-bootstrap-icons";
+import { Button, ButtonGroup, OverlayTrigger, Popover } from "react-bootstrap";
+import { Calendar3, CaretUpFill, SkipEndFill } from "react-bootstrap-icons";
 import Draggable from "react-draggable";
 
 import { Calendar } from "components/Calendar";
@@ -9,6 +9,7 @@ import { Context } from "components/Store";
 export const Timeline = () => {
   const [{ streams }] = useContext(Context);
   const [position, setPosition] = useState(0);
+  const [zoom, setZoom] = useState(1);
   const [showCal, setShowCal] = useState(false);
   const nodeRef = useRef(null);
   return (
@@ -38,32 +39,85 @@ export const Timeline = () => {
           <Button
             ref={ref}
             variant="light"
-            className="d-flex align-items-center mr-2"
+            className="d-flex align-items-center"
             {...triggerHandler}
+            style={{ marginRight: "-0.25rem", zIndex: 1 }}
           >
             <Calendar3 />
           </Button>
         )}
       </OverlayTrigger>
-      <div className="flex-grow-1 rounded overflow-hidden position-relative">
-        <Draggable
-          axis="x"
-          nodeRef={nodeRef}
-          onDrag={(event) => setPosition(position + (event as any).movementX)}
-          position={{ x: position, y: 0 }}
-        >
-          <div
-            ref={nodeRef}
-            className="position-absolute d-flex flex-column"
-            style={{ width: "100px", top: 0, bottom: 0 }}
+      <div className="flex-grow-1 overflow-hidden position-relative">
+        <div className="position-absolute w-50 h-100">
+          <Draggable
+            axis="x"
+            nodeRef={nodeRef}
+            onDrag={(event) => {
+              setPosition(position + (event as any).movementX);
+            }}
+            onStop={() =>
+              setPosition(
+                Math.min(
+                  Math.max(position, 0),
+                  (nodeRef.current as any).clientWidth
+                )
+              )
+            }
+            position={{ x: position, y: 0 }}
           >
-            {Array.from(streams.values()).map((stream) => (
-              <div key={stream.id} className="flex-grow-1 bg-light" />
-            ))}
-          </div>
-        </Draggable>
+            <div
+              ref={nodeRef}
+              className="position-absolute h-100 d-flex flex-column"
+              style={{
+                width: `${200 * zoom}%`,
+                right: 0,
+                ...(position === 0 ||
+                position === (nodeRef.current as any).clientWidth
+                  ? { transition: "transform 250ms" }
+                  : {}),
+              }}
+            >
+              {Array.from(streams.values()).map((stream) => (
+                <div key={stream.id} className="flex-grow-1 bg-primary" />
+              ))}
+            </div>
+          </Draggable>
+        </div>
+        <CaretUpFill
+          className="text-danger position-absolute m-auto"
+          style={{ bottom: "-0.3em", left: "0", right: "0" }}
+        />
       </div>
-      <Button variant="light" className="d-flex align-items-center ml-2">
+      <ButtonGroup vertical style={{ marginLeft: "-0.25rem", zIndex: 1 }}>
+        <Button
+          variant="light"
+          size="sm"
+          className="py-0"
+          disabled={zoom >= 32}
+          onClick={() => setZoom(zoom * 2)}
+          style={{ borderTopRightRadius: 0 }}
+        >
+          +
+        </Button>
+        <Button
+          variant="light"
+          size="sm"
+          className="py-0"
+          disabled={zoom <= 0.25}
+          onClick={() => setZoom(zoom * 0.5)}
+          style={{ borderBottomRightRadius: 0 }}
+        >
+          -
+        </Button>
+      </ButtonGroup>
+      <Button
+        variant="light"
+        className="d-flex align-items-center"
+        disabled={position === 0}
+        onClick={() => setPosition(0)}
+        title="Go live"
+        style={{ borderBottomLeftRadius: 0, borderTopLeftRadius: 0 }}
+      >
         <SkipEndFill />
       </Button>
     </div>
