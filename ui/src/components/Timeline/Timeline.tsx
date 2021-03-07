b4a4d14271c2da3c7fa5bc97d4db097b4da1e1aa
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-03-07 14:43:42 -0500

    Implement timeline lazy-loading

--- ui/src/components/Timeline/Timeline.tsx
+++ ui/src/components/Timeline/Timeline.tsx
@@ -1,18 +1,77 @@
 import { useContext, useRef, useState } from "react";
 import { Button, ButtonGroup, OverlayTrigger, Popover } from "react-bootstrap";
 import { Calendar3, CaretUpFill, SkipEndFill } from "react-bootstrap-icons";
-import Draggable from "react-draggable";
+import { DraggableCore } from "react-draggable";
 
 import { Calendar } from "components/Calendar";
 import { Context } from "components/Store";
+import { useInterval } from "hooks";
+
+const withoutTime = (date: Date) =>
+  new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+const getPositionFromDate = (date: Date, maxPosition: number) => {
+  const msOnly = date.getTime() - withoutTime(date).getTime();
+  return (msOnly / 8.64e7) * maxPosition;
+};
+
+const getDateFromPosition = (
+  position: number,
+  maxPosition: number,
+  date: Date
+) => {
+  const msOnly = (position / maxPosition) * 8.64e7;
+  return new Date(withoutTime(date).getTime() + msOnly);
+};
 
 export const Timeline = () => {
   const [{ streams }] = useContext(Context);
-  const [date, setDate] = useState(new Date());
-  const [position, setPosition] = useState(0);
+
+  const containerRef = useRef(null);
+  const draggerRef = useRef(null);
+
+  const now = new Date();
+  const today = withoutTime(now);
+
+  const [date, setDate] = useState(now);
   const [zoom, setZoom] = useState(1);
   const [showCal, setShowCal] = useState(false);
-  const nodeRef = useRef(null);
+
+  const draggerWidth = (containerRef.current as any)?.clientWidth * zoom;
+
+  useInterval(() => {
+    setDate(new Date(date.getTime() + 1000));
+  }, 1000);
+
+  const getStreamDivsForDate = (date: Date) => {
+    if (withoutTime(date) < today) {
+      return (
+        <>
+          {Array.from(streams.values()).map((stream) => (
+            <div
+              key={stream.id}
+              className="flex-grow-1 bg-primary text-light small"
+            />
+          ))}
+        </>
+      );
+    } else if (withoutTime(date).getTime() === today.getTime()) {
+      return (
+        <>
+          {Array.from(streams.values()).map((stream) => (
+            <div
+              key={stream.id}
+              className="flex-grow-1 bg-primary text-light small"
+              style={{ width: `${getPositionFromDate(now, 1) * 100}%` }}
+            />
+          ))}
+        </>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <ButtonGroup className="bg-secondary flex-grow-1 rounded d-flex">
       <OverlayTrigger
@@ -24,7 +83,7 @@ export const Timeline = () => {
         overlay={
           <Popover id="calendar">
             <Calendar
-              date={date}
+              date={withoutTime(date)}
               onClickDate={(date) => {
                 setDate(date);
                 setShowCal(false);
@@ -45,45 +104,73 @@ export const Timeline = () => {
           </Button>
         )}
       </OverlayTrigger>
-      <div className="flex-grow-1 overflow-hidden position-relative">
-        <Draggable
-          axis="x"
-          nodeRef={nodeRef}
+      <div
+        ref={containerRef}
+        className="flex-grow-1 overflow-hidden position-relative"
+      >
+        <DraggableCore
+          nodeRef={draggerRef}
           onDrag={(event) => {
-            setPosition(position + (event as any).movementX);
-          }}
-          onStop={() =>
-            setPosition(
-              Math.min(
-                Math.max(position, 0),
-                (nodeRef.current as any).clientWidth
+            setDate(
+              new Date(
+                Math.min(
+                  getDateFromPosition(
+                    getPositionFromDate(date, draggerWidth) -
+                      (event as any)?.movementX,
+                    draggerWidth,
+                    date
+                  ).getTime(),
+                  now.getTime()
+                )
               )
-            )
-          }
-          position={{ x: position, y: 0 }}
+            );
+          }}
         >
           <div
-            ref={nodeRef}
-            className="position-absolute h-100 d-flex flex-column"
+            ref={draggerRef}
+            className="position-absolute h-100"
             style={{
-              width: `${200 * zoom}%`,
-              right: "50%",
-              ...(position === 0 ||
-              position === (nodeRef.current as any).clientWidth
-                ? { transition: "transform 250ms" }
-                : {}),
+              width: `${100 * zoom}%`,
+              left: `calc(50% - ${Math.round(
+                getPositionFromDate(date, draggerWidth)
+              )}px)`,
             }}
           >
-            {Array.from(streams.values()).map((stream) => (
-              <div key={stream.id} className="flex-grow-1 bg-primary" />
+            {[
+              new Date(date.getTime() - 8.64e7 * 2),
+              new Date(date.getTime() - 8.64e7),
+              date,
+              new Date(date.getTime() + 8.64e7),
+              new Date(date.getTime() + 8.64e7 * 2),
+            ].map((date, i) => (
+              <div
+                key={date.toLocaleDateString()}
+                className="w-100 h-100 position-absolute d-flex flex-column"
+                style={{
+                  left: `${(i - 2) * draggerWidth}px`,
+                }}
+              >
+                {getStreamDivsForDate(date)}
+              </div>
             ))}
             {/*TODO: Add ticks*/}
           </div>
-        </Draggable>
+        </DraggableCore>
         <CaretUpFill
           className="text-light position-absolute m-auto"
-          style={{ bottom: "-0.5em", left: "0", right: "0" }}
+          style={{
+            bottom: "-0.5em",
+            left: "0",
+            right: "0",
+            pointerEvents: "none",
+          }}
         />
+        <span
+          className="text-light position-absolute text-center small"
+          style={{ top: 0, left: "0", right: "0", pointerEvents: "none" }}
+        >
+          {date.toLocaleString()}
+        </span>
         <ButtonGroup
           className="position-absolute mr-2 mt-1"
           style={{ right: 0 }}
@@ -95,7 +182,6 @@ export const Timeline = () => {
             disabled={zoom <= 0.25}
             onClick={() => {
               setZoom(zoom * 0.5);
-              setPosition(position * 0.5);
             }}
           >
             <span style={{ lineHeight: 1 }}>-</span>
@@ -107,7 +193,6 @@ export const Timeline = () => {
             disabled={zoom >= 32}
             onClick={() => {
               setZoom(zoom * 2);
-              setPosition(position * 2);
             }}
           >
             <span style={{ lineHeight: 1 }}>+</span>
@@ -117,8 +202,8 @@ export const Timeline = () => {
       <Button
         variant="light"
         className="flex-grow-0 d-flex align-items-center"
-        disabled={position === 0}
-        onClick={() => setPosition(0)}
+        disabled={now.getTime() - date.getTime() < 1}
+        onClick={() => setDate(now)}
         title="Go live"
         style={{
           marginLeft: "-0.25rem",
