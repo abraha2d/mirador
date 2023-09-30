e5334ee855f54f14ce683ca688613d1060d56794
Kevin Abraham <abraha2d@users.noreply.github.com>
2023-09-30 16:29:20 -0400

    Turn off debug by default

--- mirador/settings.py
+++ mirador/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "lo)5udl(8!rvyzz))afxjpqx#l0z(c8=!&2*isoeo&%!&_ma)s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
--- ui/src/components/StreamContainer/utils.ts
+++ ui/src/components/StreamContainer/utils.ts
@@ -1,7 +1,4 @@
 import { Stream, Video } from "../Store/types";
-import ReactHlsPlayer from "react-hls-player";
-import React, { RefObject } from "react";
-import HlsJs from "hls.js";
 
 export const isVideo = (source: Stream | Video | undefined): source is Video =>
   !!source && "file" in source;
